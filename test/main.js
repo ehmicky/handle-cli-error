5c15bced77b2d210d27399536bbb1290edaf8522
ehmicky <ehmicky@gmail.com>
2022-07-17 21:17:16 +0200

    Improve test

--- test/main.js
+++ test/main.js
@@ -3,7 +3,10 @@ import test from 'ava'
 import { handleError } from './helpers/main.js'
 
 test.serial('Dummy test', (t) => {
-  const { exitCode, consoleMessage } = handleError()
-  t.is(exitCode, 1)
+  const { consoleMessage, exitCode, exitCodeBefore, exitCodeAfter } =
+    handleError()
   t.true(consoleMessage.includes('Error: undefined'))
+  t.is(exitCode, 1)
+  t.is(exitCodeBefore, undefined)
+  t.is(exitCodeAfter, 1)
 })
