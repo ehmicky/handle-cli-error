2a94124064e524e569e2a8effcc6d864abb79f94
ehmicky <ehmicky@gmail.com>
2022-07-17 21:17:48 +0200

    Add more tests

---
+++ test/exit.js
@@ -0,0 +1,21 @@
+import test from 'ava'
+
+// eslint-disable-next-line no-restricted-imports
+import { DEFAULT_EXIT_CODE } from '../src/exit.js'
+
+import { handleError } from './helpers/main.js'
+
+test.serial('Default exit code', (t) => {
+  const { exitCode, exitCodeAfter } = handleError()
+  t.is(exitCode, DEFAULT_EXIT_CODE)
+  t.is(exitCodeAfter, DEFAULT_EXIT_CODE)
+})
+
+test.serial('Custom exit code', (t) => {
+  const customExitCode = 2
+  const { exitCode, exitCodeAfter } = handleError('', {
+    exitCode: customExitCode,
+  })
+  t.is(exitCode, customExitCode)
+  t.is(exitCodeAfter, customExitCode)
+})
