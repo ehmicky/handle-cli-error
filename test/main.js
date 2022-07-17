333ddd01286ad493d0ac50e2e74829e0c5c898b8
ehmicky <ehmicky@gmail.com>
2022-07-17 21:24:53 +0200

    Add more tests

--- test/main.js
+++ test/main.js
@@ -1,12 +1,20 @@
 import test from 'ava'
+import { each } from 'test-each'
 
 import { handleError } from './helpers/main.js'
 
-test.serial('Smoke test', (t) => {
-  const { consoleMessage, exitCode, exitCodeBefore, exitCodeAfter } =
-    handleError()
-  t.true(consoleMessage.includes('Error: undefined'))
-  t.is(exitCode, 1)
-  t.is(exitCodeBefore, undefined)
-  t.is(exitCodeAfter, 1)
-})
+each(
+  [
+    { error: undefined, expectedMessage: 'Error: undefined' },
+    // eslint-disable-next-line unicorn/no-null
+    { error: null, expectedMessage: 'Error: null' },
+    { error: 'test', expectedMessage: 'Error: test' },
+    { error: new TypeError('test'), expectedMessage: 'TypeError: test' },
+  ],
+  ({ title }, { error, expectedMessage }) => {
+    test.serial(`Normalize error | ${title}`, (t) => {
+      const { consoleMessage } = handleError(error)
+      t.true(consoleMessage.startsWith(expectedMessage))
+    })
+  },
+)
