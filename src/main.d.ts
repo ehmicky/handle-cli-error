095a3435c9fbc6f8c683b10bd67ff7f5057dcdca
ehmicky <ehmicky@gmail.com>
2022-07-17 21:25:49 +0200

    Improve types

--- src/main.d.ts
+++ src/main.d.ts
@@ -1,10 +1,44 @@
-/**
- *
- * @example
- * ```js
- * ```
- */
-export interface Options {}
+export interface Options {
+  /**
+   *
+   * @example
+   * ```js
+   * ```
+   */
+  readonly exitCode?: number
+
+  /**
+   *
+   * @example
+   * ```js
+   * ```
+   */
+  readonly short?: boolean
+
+  /**
+   *
+   * @example
+   * ```js
+   * ```
+   */
+  readonly silent?: boolean
+
+  /**
+   *
+   * @example
+   * ```js
+   * ```
+   */
+  readonly timeout?: number
+
+  /**
+   *
+   * @example
+   * ```js
+   * ```
+   */
+  readonly types?: Record<`${string}Error` | 'default', Omit<Options, 'types'>>
+}
 
 /**
  *
--- src/main.test-d.ts
+++ src/main.test-d.ts
@@ -1,7 +1,39 @@
-import { expectType } from 'tsd'
+import {
+  expectType,
+  expectError,
+  expectAssignable,
+  expectNotAssignable,
+} from 'tsd'
 
-import handleCliError from './main.js'
+import handleCliError, { Options } from './main.js'
 
 expectType<void>(handleCliError(new Error('test')))
 handleCliError('error')
 handleCliError(undefined)
+
+handleCliError('', {})
+expectAssignable<Options>({})
+
+handleCliError('', { exitCode: 0 })
+expectAssignable<Options>({ exitCode: 0 })
+expectError(handleCliError('', { exitCode: '0' }))
+expectNotAssignable<Options>({ exitCode: '0' })
+
+handleCliError('', { short: true })
+expectAssignable<Options>({ short: true })
+expectError(handleCliError('', { short: 'true' }))
+expectNotAssignable<Options>({ short: 'true' })
+
+handleCliError('', { silent: true })
+expectAssignable<Options>({ silent: true })
+expectError(handleCliError('', { silent: 'true' }))
+expectNotAssignable<Options>({ silent: 'true' })
+
+handleCliError('', { timeout: 0 })
+expectAssignable<Options>({ timeout: 0 })
+handleCliError('', { timeout: 1e3 })
+expectAssignable<Options>({ timeout: 1e3 })
+handleCliError('', { timeout: Number.POSITIVE_INFINITY })
+expectAssignable<Options>({ timeout: Number.POSITIVE_INFINITY })
+expectError(handleCliError('', { timeout: '0' }))
+expectNotAssignable<Options>({ timeout: '0' })
