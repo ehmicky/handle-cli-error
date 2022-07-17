a35fc4f8598823428fb97ac90877563f6c7def39
ehmicky <ehmicky@gmail.com>
2022-07-17 21:17:23 +0200

    Rename test

--- test/main.js
+++ test/main.js
@@ -2,7 +2,7 @@ import test from 'ava'
 
 import { handleError } from './helpers/main.js'
 
-test.serial('Dummy test', (t) => {
+test.serial('Smoke test', (t) => {
   const { consoleMessage, exitCode, exitCodeBefore, exitCodeAfter } =
     handleError()
   t.true(consoleMessage.includes('Error: undefined'))
