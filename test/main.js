61170c2df310a9080300d42066dce86d78a41d80
ehmicky <ehmicky@gmail.com>
2022-10-02 19:48:37 +0200

    Fix linting

--- test/main.js
+++ test/main.js
@@ -7,7 +7,6 @@ import { handleError } from './helpers/main.js'
 each(
   [
     { error: undefined, expectedMessage: 'Error: undefined' },
-    // eslint-disable-next-line unicorn/no-null
     { error: null, expectedMessage: 'Error: null' },
     { error: 'test', expectedMessage: 'Error: test' },
     { error: new TypeError('test'), expectedMessage: 'TypeError: test' },
