1d558358b8d8dadc5bdd5985acaa1ff4bf5572aa
ehmicky <ehmicky@gmail.com>
2022-10-02 11:16:50 +0200

    Browser support

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+# 2.1.0
+
+## Features
+
+- Browsers support
+
 # 2.0.0
 
 ## Breaking changes
--- test/helpers/exit.js
+++ test/helpers/exit.js
@@ -1,10 +1,25 @@
 import process from 'process'
 
-const originalProcessExit = process.exit
-const originalProcessExitCode = process.exitCode
+// eslint-disable-next-line fp/no-let, init-declarations
+let originalProcessExit
+// eslint-disable-next-line fp/no-let, init-declarations
+let originalProcessExitCode
+// eslint-disable-next-line fp/no-let, init-declarations
+let exitFuncCode
 
 // Mock `process.exit()` during tests
 export const mockProcessExit = function () {
+  // eslint-disable-next-line n/prefer-global/process
+  const { process } = globalThis
+
+  if (process === undefined) {
+    return
+  }
+
+  // eslint-disable-next-line fp/no-mutation
+  originalProcessExitCode = process.exitCode
+  // eslint-disable-next-line fp/no-mutation
+  originalProcessExit = process.exit
   // eslint-disable-next-line fp/no-mutation
   process.exit = mockedProcessExit
 }
@@ -21,12 +36,16 @@ export const getProcessExitCodes = function () {
 
 // Reverse `mockProcessExit()`
 export const unmockProcessExit = function () {
-  process.exitCode = originalProcessExitCode
+  // eslint-disable-next-line n/prefer-global/process
+  const { process } = globalThis
+
+  if (process === undefined) {
+    return
+  }
+
   // eslint-disable-next-line fp/no-mutation
   process.exit = originalProcessExit
+  process.exitCode = originalProcessExitCode
   // eslint-disable-next-line fp/no-mutation
   exitFuncCode = undefined
 }
-
-// eslint-disable-next-line fp/no-let, init-declarations
-let exitFuncCode
