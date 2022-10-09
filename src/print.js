d1ec5d0029b6b580cdfc096689cfd6e2c9b00dc5
ehmicky <ehmicky@gmail.com>
2022-10-09 17:13:01 +0200

    Update comment

--- src/print.js
+++ src/print.js
@@ -1,11 +1,9 @@
 // Print the error.
-// If `stack: false`, we only print the error `message`, which is useful for
-// well-known errors such as input validation.
+// If `stack: false`, we do not print the error `stack` nor inline preview,
+// which is useful for well-known errors such as input validation.
 // Otherwise, we pass the `error` instance to `console.error()`, so it prints
 // not only its `message` and `stack` but also its properties, `cause`,
-// aggregate `errors`, add colors, etc.
-//  - In Node.js, this is done by `util.inspect()`
-//  - In browsers, this is also done, but differently
+// aggregate `errors`, add colors, inline preview, etc. using `util.inspect()`.
 export const printError = function (error, silent, stack) {
   if (silent) {
     return
