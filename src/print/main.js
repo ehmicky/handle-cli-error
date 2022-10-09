7c782655295cfb17fdb3b363d9d40836d1ea71c9
ehmicky <ehmicky@gmail.com>
2022-10-09 17:27:39 +0200

    Split file

--- src/print/main.js
+++ src/print/main.js
@@ -1,8 +1,7 @@
 import { inspect } from 'util'
 
-import isErrorInstance from 'is-error-instance'
-
 import { omitProps } from './props.js'
+import { omitStack, restoreStack, omitStackBracket } from './stack.js'
 
 // Print the error.
 // If `stack: false`, we do not print the error `stack` nor inline preview,
@@ -24,88 +23,7 @@ const serializeError = function ({ error, stack, props, colors }) {
   const errorA = omitProps(error, props)
   omitStack(errorA, stack)
   const errorString = inspect(errorA, { colors })
-  const errorStringA = omitStackBracket(errorString, stack)
   restoreStack(errorA, stack)
+  const errorStringA = omitStackBracket(errorString, stack)
   return errorStringA
 }
-
-// If `stack` is `false`, we do not print stack traces.
-// We do it by temporarily removing `error.stack` recursively.
-// We prefer this over using some string replacement logic since this is less
-// brittle.
-const omitStack = function (error, stack) {
-  if (!stack) {
-    recurseObject(error, omitStackProp)
-  }
-}
-
-const omitStackProp = function (object) {
-  if (
-    !isErrorInstance(object) ||
-    typeof object.stack !== 'string' ||
-    object.stack === ''
-  ) {
-    return
-  }
-
-  setNonEnumProp(object, STACK_SYM, object.stack)
-  // eslint-disable-next-line fp/no-delete, no-param-reassign
-  delete object.stack
-}
-
-const restoreStack = function (error, stack) {
-  if (!stack) {
-    recurseObject(error, restoreStackProp)
-  }
-}
-
-const restoreStackProp = function (object) {
-  if (object[STACK_SYM] === undefined) {
-    return
-  }
-
-  setNonEnumProp(object, 'stack', object[STACK_SYM])
-  // eslint-disable-next-line fp/no-delete, no-param-reassign
-  delete object[STACK_SYM]
-}
-
-const STACK_SYM = Symbol('stack')
-
-const setNonEnumProp = function (object, propName, value) {
-  // eslint-disable-next-line fp/no-mutating-methods
-  Object.defineProperty(object, propName, {
-    value,
-    enumerable: false,
-    writable: true,
-    configurable: true,
-  })
-}
-
-// Calls `callFunc(object)` on any object, recursively
-const recurseObject = function (value, callFunc) {
-  recurseValue(value, callFunc, [])
-}
-
-const recurseValue = function (value, callFunc, parents) {
-  if (typeof value !== 'object' || value === null || parents.includes(value)) {
-    return
-  }
-
-  callFunc(value)
-
-  Reflect.ownKeys(value).forEach((key) => {
-    recurseValue(value[key], callFunc, [...parents, value])
-  })
-}
-
-// `util.inspect()` surround `error.name: error.message` with `[...]` when
-// `error.stack` is missing. This is unwanted so we remove it.
-// This is only removed for the top-level error since nested errors:
-//  - Are less frequent
-//  - Do not look as weird with `[...]`
-//  - Would be harder to fix
-const omitStackBracket = function (errorString, stack) {
-  return stack ? errorString : errorString.replace(STACK_BRACKET_REGEXP, '$1')
-}
-
-const STACK_BRACKET_REGEXP = /^\[([^\]]+)\]/u
---
+++ src/print/stack.js
@@ -0,0 +1,82 @@
+import isErrorInstance from 'is-error-instance'
+
+// If `stack` is `false`, we do not print stack traces.
+// We do it by temporarily removing `error.stack` recursively.
+// We prefer this over using some string replacement logic since this is less
+// brittle.
+export const omitStack = function (error, stack) {
+  if (!stack) {
+    recurseObject(error, omitStackProp)
+  }
+}
+
+const omitStackProp = function (object) {
+  if (
+    !isErrorInstance(object) ||
+    typeof object.stack !== 'string' ||
+    object.stack === ''
+  ) {
+    return
+  }
+
+  setNonEnumProp(object, STACK_SYM, object.stack)
+  // eslint-disable-next-line fp/no-delete, no-param-reassign
+  delete object.stack
+}
+
+export const restoreStack = function (error, stack) {
+  if (!stack) {
+    recurseObject(error, restoreStackProp)
+  }
+}
+
+const restoreStackProp = function (object) {
+  if (object[STACK_SYM] === undefined) {
+    return
+  }
+
+  setNonEnumProp(object, 'stack', object[STACK_SYM])
+  // eslint-disable-next-line fp/no-delete, no-param-reassign
+  delete object[STACK_SYM]
+}
+
+const STACK_SYM = Symbol('stack')
+
+const setNonEnumProp = function (object, propName, value) {
+  // eslint-disable-next-line fp/no-mutating-methods
+  Object.defineProperty(object, propName, {
+    value,
+    enumerable: false,
+    writable: true,
+    configurable: true,
+  })
+}
+
+// Calls `callFunc(object)` on any object, recursively
+const recurseObject = function (value, callFunc) {
+  recurseValue(value, callFunc, [])
+}
+
+const recurseValue = function (value, callFunc, parents) {
+  if (typeof value !== 'object' || value === null || parents.includes(value)) {
+    return
+  }
+
+  callFunc(value)
+
+  Reflect.ownKeys(value).forEach((key) => {
+    recurseValue(value[key], callFunc, [...parents, value])
+  })
+}
+
+// `util.inspect()` surround `error.name: error.message` with `[...]` when
+// `error.stack` is missing. This is unwanted so we remove it.
+// This is only removed for the top-level error since nested errors:
+//  - Are less frequent
+//  - Do not look as weird with `[...]`
+//  - Would be harder to fix
+export const omitStackBracket = function (errorString, stack) {
+  return stack ? errorString : errorString.replace(STACK_BRACKET_REGEXP, '$1')
+}
+
+const STACK_BRACKET_REGEXP = /^\[([^\]]+)\]/u
