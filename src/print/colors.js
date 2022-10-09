ef1e8aa52d294fc3e4b5b096a00d05c0b67e2f75
ehmicky <ehmicky@gmail.com>
2022-10-09 17:58:14 +0200

    Add quotes colors

--- src/print/colors.js
+++ src/print/colors.js
@@ -10,7 +10,39 @@ export const getColors = function (colors) {
   return { chalk, useColors }
 }
 
-// When `colors` is true, add colors to quoted strings
+// When `colors` is true, add colors to quoted strings.
 export const colorizeError = function (errorString, chalk, useColors) {
+  if (!useColors) {
+    return errorString
+  }
+
   return errorString
+    .replace(
+      DOUBLE_QUOTED_STRING,
+      colorizeQuotedString.bind(undefined, chalk, 'bold'),
+    )
+    .replace(
+      SINGLE_QUOTED_STRING,
+      colorizeQuotedString.bind(undefined, chalk, 'bold'),
+    )
+    .replace(
+      BACKTICK_QUOTED_STRING,
+      colorizeQuotedString.bind(undefined, chalk, 'italic'),
+    )
+}
+
+const DOUBLE_QUOTED_STRING = /(")([^"]+)(")/gu
+const SINGLE_QUOTED_STRING = /(')([^']+)(')/gu
+const BACKTICK_QUOTED_STRING = /(`)([^`]+)(`)/gu
+
+// eslint-disable-next-line max-params
+const colorizeQuotedString = function (
+  chalk,
+  style,
+  fullString,
+  startQuote,
+  quotedString,
+  endQuote,
+) {
+  return `${startQuote}${chalk[style](quotedString)}${endQuote}`
 }
