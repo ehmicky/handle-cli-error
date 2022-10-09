4855a27f6ce32b99055fba07aba85594201530f4
ehmicky <ehmicky@gmail.com>
2022-10-09 17:29:48 +0200

    Refactoring

--- test/helpers/main.js
+++ test/helpers/main.js
@@ -7,16 +7,19 @@ import { mockTimeout, unmockTimeout, advanceTimeout } from './timeout.js'
 
 // eslint-disable-next-line no-restricted-globals
 sinon.stub(console, 'error')
+sinon.stub(process, 'exit')
 
 // `handle-cli-error` use global variables `process.exitCode`, `process.exit()`
 // and `console.error()` so we need to mock them.
 // It also relies on timeout, which we need to mock as well.
 // eslint-disable-next-line max-statements
 export const handleError = function (error, options) {
-  const clock = mockAll()
+  const clock = mockTimeout()
 
   try {
-    resetConsoleMock()
+    // eslint-disable-next-line no-restricted-globals, no-console
+    console.error.resetHistory()
+    process.exit.resetHistory()
     handleCliError(error, options)
     // eslint-disable-next-line no-restricted-globals, no-console
     const consoleArg = getStubArg(console.error)
@@ -26,33 +29,9 @@ export const handleError = function (error, options) {
     const exitCodeAfter = getStubArg(exit)
     return { consoleArg, exitCode, exitCodeBefore, exitCodeAfter }
   } finally {
-    unmockAll(clock)
+    process.exitCode = undefined
+    unmockTimeout(clock)
   }
-}
-
-const mockAll = function () {
-  const clock = mockTimeout()
-  mockProcessExit()
-  return clock
-}
-
-const unmockAll = function (clock) {
-  unmockProcessExit()
-  unmockTimeout(clock)
-}
-
-const mockProcessExit = function () {
-  sinon.stub(process, 'exit')
-}
-
-const unmockProcessExit = function () {
-  process.exit.restore()
-  process.exitCode = undefined
-}
-
-const resetConsoleMock = function () {
-  // eslint-disable-next-line no-restricted-globals, no-console
-  console.error.resetHistory()
 }
 
 const getStubArg = function (stub) {
