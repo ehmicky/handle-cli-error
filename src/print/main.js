58280550e70b0f0491a9a8ef22722320d96d03cb
ehmicky <ehmicky@gmail.com>
2022-10-09 17:27:34 +0200

    Split file

--- src/print/main.js
+++ src/print/main.js
@@ -2,6 +2,8 @@ import { inspect } from 'util'
 
 import isErrorInstance from 'is-error-instance'
 
+import { omitProps } from './props.js'
+
 // Print the error.
 // If `stack: false`, we do not print the error `stack` nor inline preview,
 // which is useful for well-known errors such as input validation.
@@ -25,42 +27,6 @@ const serializeError = function ({ error, stack, props, colors }) {
   const errorStringA = omitStackBracket(errorString, stack)
   restoreStack(errorA, stack)
   return errorStringA
-}
-
-// If `props` is `false`, we do not print error properties.
-//  - Including `cause` and `errors`
-// This requires creating a copy of `error` with only `name`, `message`,
-// `stack` and `__proto__`. This allows using using `util.inspect()` to:
-//  - Keep stack trace colors
-//  - Keep `error.name` output consistent regardless of `props` and `stack`
-//    since `util.inspect()` sometimes print it differently
-const omitProps = function (error, props) {
-  if (props) {
-    return error
-  }
-
-  const errorCopy = new Error('')
-  // eslint-disable-next-line fp/no-mutating-methods
-  Object.setPrototypeOf(errorCopy, Object.getPrototypeOf(error))
-  copyDescriptors(errorCopy, error)
-  return errorCopy
-}
-
-const copyDescriptors = function (errorCopy, error) {
-  COPIED_PROPS.forEach((propName) => {
-    copyDescriptor(errorCopy, error, propName)
-  })
-}
-
-const COPIED_PROPS = ['name', 'message', 'stack']
-
-const copyDescriptor = function (errorCopy, error, propName) {
-  const descriptor = Object.getOwnPropertyDescriptor(error, propName)
-
-  if (descriptor !== undefined) {
-    // eslint-disable-next-line fp/no-mutating-methods
-    Object.defineProperty(errorCopy, propName, descriptor)
-  }
 }
 
 // If `stack` is `false`, we do not print stack traces.
