d573eb2d9c3152d5595b2cd398f3a1810675c1f2
ehmicky <ehmicky@gmail.com>
2025-05-18 13:38:57 +0100

    Fix tests

--- src/helpers/main.test.js
+++ src/helpers/main.test.js
@@ -6,43 +6,47 @@ import { stub } from 'sinon'
 
 import { DEFAULT_TIMEOUT, INFINITE_TIMEOUT, NO_TIMEOUT } from '../timeout.js'
 
-// eslint-disable-next-line no-restricted-globals
-stub(console, 'error')
-stub(process, 'exit')
-const clock = install()
-
 // `handle-cli-error` use global variables `process.exitCode`, `process.exit()`
 // and `console.error()` so we need to mock them.
 // It also relies on timeout, which we need to mock as well.
 export const handleError = (error, options) => {
+  const clock = startMocks()
+
   try {
-    resetMocks()
     handleCliError(error, options)
     // eslint-disable-next-line no-restricted-globals, no-console
     const consoleArg = getStubArg(console.error)
-    const processExitArgs = getProcessExitArgs(options)
+    const processExitArgs = getProcessExitArgs(clock, options)
     return { consoleArg, ...processExitArgs }
   } finally {
-    process.exitCode = undefined
+    stopMocks(clock)
   }
 }
 
-const resetMocks = () => {
-  // eslint-disable-next-line no-restricted-globals, no-console
-  console.error.resetHistory()
-  process.exit.resetHistory()
-  clock.reset()
+const startMocks = () => {
+  // eslint-disable-next-line no-restricted-globals
+  stub(console, 'error')
+  stub(process, 'exit')
+  return install()
 }
 
-const getProcessExitArgs = (options) => {
+const stopMocks = (clock) => {
+  clock.uninstall()
+  process.exit.restore()
+  process.exitCode = undefined
+  // eslint-disable-next-line no-restricted-globals, no-console
+  console.error.restore()
+}
+
+const getProcessExitArgs = (clock, options) => {
   const { exitCode, exit } = process
   const exitCodeBefore = getStubArg(exit)
-  advanceTimeout(options)
+  advanceTimeout(clock, options)
   const exitCodeAfter = getStubArg(exit)
   return { exitCode, exitCodeBefore, exitCodeAfter }
 }
 
-const advanceTimeout = ({ timeout = DEFAULT_TIMEOUT } = {}) => {
+const advanceTimeout = (clock, { timeout = DEFAULT_TIMEOUT } = {}) => {
   if (timeout !== NO_TIMEOUT && timeout !== INFINITE_TIMEOUT && timeout >= 0) {
     clock.tick(timeout)
   }
